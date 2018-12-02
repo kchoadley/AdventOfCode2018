34c9c4239756279212610ad4723de2b41cbd006e
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-12-02 17:59:50 -0600

    Solution to day 1

--- day01/src/main.cpp
+++ day01/src/main.cpp
@@ -1,16 +1,58 @@
-
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <set>
+#include <vector>
+
+// Forward declarations
+std::vector<int> load(std::ifstream& input);
+int part1(std::vector<int> inputs);
+int part2(std::vector<int> inputs);
 
 int main(int argc, const char * argv[]) {
-    std::cout << "I'm alive!" << std::endl;
-
     std::ifstream input ("../input.txt");
+    std::vector<int> data (load(input));
+    input.close();
+
+    int offset = part1(data);
+    int duplicate_frequency (part2(data));
+
+    std::cout << "The result is: " << offset << std::endl;
+    std::cout << "The duplicate frequency is: " << duplicate_frequency << std::endl;
+}
+std::vector<int> load(std::ifstream& input) {
+    std::vector<int> data;
     std::string line;
     while (getline(input, line)) {
-        std::cout << line << std::endl;
+        if (line.at(0) == '-'){
+            data.push_back(std::stoi(line.substr(0)));
+        } else if (line.at(0) == '+'){
+            data.push_back(std::stoi(line.substr(1)));
+        } else {
+            std::cerr << "Bad input: " << line << std::endl;
+            exit(1);
+        }
     }
-    input.close();
-    std::cout << "Good night!" << std::endl;
+    return data;
+}
+int part1(std::vector<int> inputs) {
+    int offset = 0;
+    for (const auto& number : inputs) {
+        offset += number;
+    }
+    return offset;
+}
+int part2(std::vector<int> inputs) {
+    std::set<int> frequencies { 0 };
+    int frequency = 0;
+    while (true) {
+        for (const auto& number : inputs) {
+            frequency += number;
+            if (frequencies.find(frequency) == frequencies.end()) {
+                frequencies.insert(frequency);
+            } else {
+                return frequency;
+            }
+        }
+    }
 }
